7a8239d2cbb65de53a08fdaec9988ed21ecfb6d7
root <root@nova-1.lyon.grid5000.fr>
2019-05-17 14:53:21 +0200

    Add: WordCount.scala

---
+++ src/main/scala/WordCount.scala
@@ -0,0 +1,14 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+object SparkWordCount {
+  def main(args: Array[String]) {
+    val sc = new SparkContext(new SparkConf().setAppName("WordCount benchmark"))
+    val text = sc.textFile("input.txt") 
+    val counts = text.flatMap(line => line.split(" "))
+      .map(word => (word, 1))
+      .reduceByKey(_ + _)
+    counts.saveAsTextFile("outfile")
+    System.out.println("OK");
+  }
+}
