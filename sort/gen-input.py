c534d8051f916517e3396dc9428d69be7f07e590
David Guyon <davidguyon49@gmail.com>
2019-06-28 15:03:26 +0200

    Add: gen-input is using RAM for faster execution

--- sort/gen-input.py
+++ sort/gen-input.py
@@ -1,11 +1,11 @@
-#!/bin/python
+#!/usr/bin/python
 
 import os
 import sys
 from random import seed
 from random import randint
 
-desired_size_mb = 10
+desired_size_mb = 50
 input_file = "input.txt"
 
 print("Checking if %s already exist" % input_file)
@@ -19,14 +19,16 @@ seed(1)
 
 print("\nCreating %s with %dMB of data" % (input_file, desired_size_mb))
 with open(input_file, 'w') as f:
+    data = ""
     last_size_mb = 0
     while True:
-        current_size_mb = f.tell() / 1024 / 1024
+        data += "%d\n" % randint(0, 1000000000)
+        current_size_mb = sys.getsizeof(data) / 1024 / 1024
         if int(current_size_mb) > int(last_size_mb):
-            print(" - %s is %dMB" % (input_file, current_size_mb))
+            print(" - %dMB of data generated" % current_size_mb)
         if current_size_mb >= desired_size_mb:
+            print(current_size_mb)
             break
-        value = randint(0, 1000000000)
-        f.write("%d\n" % value)
         last_size_mb = current_size_mb
+    f.write(data)
 
